331e7a872d88fb71475b088b4f519c42666d7d64
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-05-05 23:18:28 +0500

    реализация главной формы

---
+++ action.py
@@ -0,0 +1,123 @@
+from PyQt5.QtWidgets import QMainWindow, qApp, QApplication, QFileDialog, \
+    QWidget, \
+    QAction, QMdiArea, QMdiSubWindow, QTabWidget
+
+
+from forms import Book, Class, Clients, Provider, Shops, Worker_info
+from rel_forms import Order_of_books, Production, Salary, Supply
+
+class C_Action(QAction):
+    def __init__(self, mdi: QMdiArea = None, name: str = 'Default', parent=None, ):
+        super(C_Action, self).__init__(name, parent)
+        self.mdi = mdi
+
+    def set_params(self, shortcut: str, tittle=None, obj=None):
+        self.title = tittle
+        self.obj = obj
+        self.setShortcut(shortcut)
+        self.triggered.connect(self.act)
+
+    def act(self):
+        print(dir(self))
+        sub = QMdiSubWindow()
+        sub.setWidget(self.obj())
+        sub.setWindowTitle(self.title)
+        self.mdi.addSubWindow(sub)
+        sub.show()
+        print(dir(self))
+
+
+
+class Action(QAction):
+    def __init__(self, name:str, parent, tabs):
+        super(Action, self).__init__(name, parent)
+        self.tabs = tabs
+
+    def set_params(self, shortcut: str,  cls=None):
+        self.setShortcut(shortcut)
+
+        self.cls = cls
+        self.triggered.connect(self.act)
+
+    def act(self):
+        # print(dir(self))
+        obj = self.cls()
+        self.tabs.addTab(obj, obj.name)
+
+        # print(dir(self))
+
+class Book_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Книги', parent, tabs)
+        self.set_params(shortcut='Ctrl+B', cls=Book)
+
+class Class_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Класс обслуживания', parent, tabs)
+        self.set_params(shortcut='Ctrl+K', cls=Class)
+
+class Clients_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Клиенты', parent, tabs)
+        self.set_params(shortcut='Ctrl+L', cls=Clients)
+
+class Provider_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Поставщики', parent, tabs)
+        self.set_params(shortcut='Ctrl+P', cls=Provider)
+
+class Shops_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Магазины', parent, tabs)
+        self.set_params(shortcut='Ctrl+S', cls=Shops)
+
+class Worker_info_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Информация о сотрудниках', parent, tabs)
+        self.set_params(shortcut='Ctrl+W', cls=Worker_info)
+
+
+class Order_of_books_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Заказы', parent, tabs)
+        self.set_params(shortcut='Ctrl+O', cls=Order_of_books)
+
+
+class Production_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Книги в заказе', parent, tabs)
+        self.set_params(shortcut='Ctrl+R', cls=Production)
+
+class Salary_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Зарплата', parent, tabs)
+        self.set_params(shortcut='Ctrl+R', cls=Salary)
+
+
+class Supply_act(Action):
+    def __init__(self, parent, tabs):
+        super().__init__('&Поставки', parent, tabs)
+        self.set_params(shortcut='Ctrl+U', cls=Supply)
+
+
+# class Open_action(C_Action):
+#     def __init__(self, parent):
+#         super(Open_action, self).__init__(name='&Open', parent=parent)
+#         self.set_params(shortcut='Ctrl+O')
+#
+#     def act(self):
+#         import os
+#         description = 'Загрузить hdf5'  # заголовок окна диалога
+#         default_path = os.path.dirname(os.path.abspath(__file__))  # дефолтный путь диалогового окна
+#         filter = "Таблицы hdf5  (*.hdf5)"  # фильтр расширения
+#         # получаем путь к файлу
+#         path, _ = QFileDialog.getOpenFileName(None, description, default_path, filter)
+
+
+class Exit_act(C_Action):
+    def __init__(self, parent):
+        super(Exit_act, self).__init__(name='&Exit', parent=parent)
+        self.set_params(shortcut='Ctrl+Q')
+
+    def act(self):
+        qApp.quit()
--- main.py
+++ main.py
@@ -2,59 +2,14 @@ import sys
 
 from PyQt5.QtWidgets import QMainWindow, qApp, QApplication, QFileDialog, \
     QWidget, \
-    QAction, QMdiArea, QMdiSubWindow, QVBoxLayout, QTabWidget
-
-from forms.book import Book
+    QAction, QMdiArea, QMdiSubWindow, QTabWidget
 
 
-class C_Action(QAction):
-    def __init__(self, mdi: QMdiArea = None, name: str = 'Default', parent=None, ):
-        super(C_Action, self).__init__(name, parent)
-        self.mdi = mdi
-
-    def set_params(self, shortcut: str, tittle=None, obj=None):
-        self.title = tittle
-        self.obj = obj
-        self.setShortcut(shortcut)
-        self.triggered.connect(self.act)
-
-    def act(self):
-        print(dir(self))
-        sub = QMdiSubWindow()
-        sub.setWidget(self.obj())
-        sub.setWindowTitle(self.title)
-        self.mdi.addSubWindow(sub)
-        sub.show()
-        print(dir(self))
+from forms import Book, Class, Clients, Provider, Shops, Worker_info
+from action import  Exit_act, Book_act, Class_act, Clients_act, Provider_act, Shops_act, Worker_info_act
+from action import  Order_of_books_act, Production_act, Salary_act, Supply_act
 
 
-class Book_action(C_Action):
-    def __init__(self, mdi, parent):
-        super().__init__(mdi, '&Book', parent)
-        self.set_params(shortcut='Ctrl+B', tittle='Book', obj=Book)
-
-
-class Open_action(C_Action):
-    def __init__(self, parent):
-        super(Open_action, self).__init__(name='&Open', parent=parent)
-        self.set_params(shortcut='Ctrl+O')
-
-    def act(self):
-        import os
-        description = 'Загрузить hdf5'  # заголовок окна диалога
-        default_path = os.path.dirname(os.path.abspath(__file__))  # дефолтный путь диалогового окна
-        filter = "Таблицы hdf5  (*.hdf5)"  # фильтр расширения
-        # получаем путь к файлу
-        path, _ = QFileDialog.getOpenFileName(None, description, default_path, filter)
-
-
-class Exit_action(C_Action):
-    def __init__(self, parent):
-        super(Exit_action, self).__init__(name='&Exit', parent=parent)
-        self.set_params(shortcut='Ctrl+Q')
-
-    def act(self):
-        qApp.quit()
 
 
 class Example(QMainWindow):
@@ -76,31 +31,37 @@ class Example(QMainWindow):
         self.tabs.setTabsClosable(True)
         self.tabs.setTabShape(QTabWidget.Rounded)
 
-        for i in range(10):
-            self.tabs.addTab(QWidget(), f'tab{i}')
+        # book = Book()
+        # self.tabs.addTab(book, book.name)
 
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(Open_action(self))
-        fileMenu.addAction(Exit_action(self))
+        fileMenu = menubar.addMenu('&Файл')
+        # fileMenu.addAction(Open_action(self))
+        fileMenu.addAction(Exit_act(self))
 
-        directoryMenu = menubar.addMenu('&Directory')
-        # directoryMenu.addAction(Book_action(self.mdi, self))
+        directoryMenu = menubar.addMenu('&Справочники')
+        directoryMenu.addAction(Book_act(self, self.tabs))
+        directoryMenu.addAction(Class_act(self, self.tabs))
+        directoryMenu.addAction(Clients_act(self, self.tabs))
+        directoryMenu.addAction(Provider_act(self, self.tabs))
+        directoryMenu.addAction(Shops_act(self, self.tabs))
+        directoryMenu.addAction(Worker_info_act(self, self.tabs))
 
-        reportsMenu = menubar.addMenu('&Reports')
-        QueryMenu = menubar.addMenu('&Query')
+        relationMenu = menubar.addMenu('&Журналы')
+        relationMenu.addAction(Order_of_books_act(self, self.tabs))
+        relationMenu.addAction(Production_act(self, self.tabs))
+        relationMenu.addAction(Salary_act(self, self.tabs))
+        relationMenu.addAction(Supply_act(self, self.tabs))
+
+        reportsMenu = menubar.addMenu('&Отчеты')
+        QueryMenu = menubar.addMenu('&Запросы')
 
         # self.setGeometry(300, 300, 300, 200)
         # self.showFullScreen()
 
+        self.setWindowTitle('База данных книготорговой организация')
 
-        self.setWindowTitle('Menubar')
 
-        # vbox = QVBoxLayout()
-        # vbox.addWidget(menubar)
-        # vbox.addWidget(self.tabs)
-
-        # self.setLayout(vbox)
         self.showMaximized()
         self.show()
 
@@ -112,4 +73,4 @@ class Example(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
