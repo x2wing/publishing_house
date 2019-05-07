b9174d2dc32db6cd64da822fbd8d9c11209cb0a8
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-05-07 09:56:15 +0500

    Добавлены виджеты фильтра

--- forms/directory.py
+++ forms/directory.py
@@ -1,13 +1,20 @@
 import sys
 
 from PyQt5 import QtWidgets, QtSql
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QSizePolicy
 from PyQt5 import  QtCore
 from PyQt5.QtGui import  QStandardItemModel
+from db import BOOK
+
+class ComboBox(QtWidgets.QComboBox):
+    def __init__(self, parent):
+        super().__init__(parent)
+        print('ComboBox',BOOK.__table__.columns.keys())
 
 class Directory(QWidget):
     def __init__(self, window_title, db_table, parent=None, sort_column=0, hide_column=0,  ):
         super().__init__(parent)
+        self.hide_column = hide_column
         self.setWindowTitle(window_title)
         # нужно сначала создать виджет чтобы подцепилась модель
         self.tv = QtWidgets.QTableView(self)
@@ -40,9 +47,16 @@ class Directory(QWidget):
 
     def create_ui(self, hide_column):
         vbox = QtWidgets.QVBoxLayout()
+        hbox = QtWidgets.QHBoxLayout()
+
+        column= ComboBox(self)
+        condition= QtWidgets.QComboBox(self)
+        value = QtWidgets.QLineEdit(self)
+
 
         # self.tv.setModel(self.stm)
         self.tv.hideColumn(hide_column)
+        # self.tv.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
 
         btnAdd = QtWidgets.QPushButton('&Добавить запись')
         btnAdd.clicked.connect(self.addRecord)
@@ -54,6 +68,10 @@ class Directory(QWidget):
         btnRemove = QtWidgets.QPushButton('&Удалить изменения')
         btnRemove.clicked.connect(self.removeRecords)
 
+        hbox.addWidget(column)
+        hbox.addWidget(condition)
+        hbox.addWidget(value)
+        vbox.addLayout(hbox)
         vbox.addWidget(self.tv)
         vbox.addWidget(btnAdd)
         vbox.addWidget(btnDel)
@@ -63,6 +81,10 @@ class Directory(QWidget):
         self.setLayout(vbox)
         self.resize(600, 500)
         # self.show()
+
+    def setFilter(self, column, condition, value):
+        self.stm.setFilter('worker_id=1')
+        self.stm.select()
 
     def addRecord(self):
         self.stm.insertRow(self.stm.rowCount())
@@ -102,6 +124,6 @@ class Directory(QWidget):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Directory("abstract_editor", 'book')
-
+    window.show()
     # print(QtSql.QSqlDatabase.drivers())
     sys.exit(app.exec())
