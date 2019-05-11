4c29383d1afa745d565f062860545b583bd19fd4
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-05-11 21:38:58 +0500

    добавлен импорт конфига базы данных в init пакета db

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,4 @@
 from db.database import CLASS, BOOK, CLIENTS, SHOPS, PROVIDER, \
     WORKER_INFO, SALARY, ORDER_OF_BOOKS, SUPPLY, PRODUCTION, db
 from db.reports import Order_to_worker, Client_and_class
+from db.config import conf
